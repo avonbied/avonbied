bf9e1d8134d9d8419d8360be037bd478c6b409a2
Lex <58815784+avonbied@users.noreply.github.com>
2024-04-23 21:46:13 -0500

    Update hostname lookup snippet description

--- snippets/pwsh.ps1
+++ snippets/pwsh.ps1
@@ -10,9 +10,9 @@ Select-String -Path $file -Pattern $pattern
 # Output all running/stopped services
 Get-Service | Select-Object -Property Name, DisplayName, Status, ServiceType, @{l='DependentServices';e={[string]::join(';',($_.DependentServices.Name))}}, @{l='ServicesDependedOn';e={[string]::join(';',($_.ServicesDependedOn.Name))}} | -Path "$env:COMPUTERNAME.csv"
 
-# Resolve DNS records for list of RecordNames (objects with a 'RecordName' property)
-$records | ForEach-Object {
-	$initial = Resolve-DnsName $_.RecordName
+# Resolve DNS records for list of Hostnames (objects with a 'Hostname' property)
+$hostnames | ForEach-Object {
+	$initial = Resolve-DnsName $_.Hostname
 	Add-Member -InputObject $_ -MemberType NoteProperty -Name MappedCNames -Value (($initial | Where-Object { $_.Type -eq 'CNAME' }).NameHost -join '; ')
 	Add-Member -InputObject $_ -MemberType NoteProperty -Name IPs -Value (($initial | Where-Object { $_.Type -ne 'CNAME' }).IPAddress -join '; ') -PassThru
 }
