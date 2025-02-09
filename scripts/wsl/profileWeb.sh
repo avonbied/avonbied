f8dc48bd0ba88fd28d41d219d03f5e7177e5f7e2
Lex <58815784+avonbied@users.noreply.github.com>
2025-02-08 23:11:07 -0600

    Create profileWeb.sh

---
+++ scripts/wsl/profileWeb.sh
@@ -0,0 +1,7 @@
+sudo apk add --no-cache libstdc++ coreutils
+
+# Download and install nvm:
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+echo "export NVM_NODEJS_ORG_MIRROR=https://unofficial-builds.nodejs.org/download/release" >> .profile
+echo "nvm_get_arch() { nvm_echo \"x64-musl\"; }" >> .profile
+source .profile
