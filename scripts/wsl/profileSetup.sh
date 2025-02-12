b35de9c8999ac2c5f8217090505a3e081b58bdbf
Lex <58815784+avonbied@users.noreply.github.com>
2025-02-11 19:15:45 -0600

    Update profileSetup.sh

--- scripts/wsl/profileSetup.sh
+++ scripts/wsl/profileSetup.sh
@@ -7,23 +7,14 @@ curl -o /etc/bash/git-completion.bash https://raw.githubusercontent.com/git/git/
 # Create User
 adduser -G wheel -s /bin/bash $1
 echo -e "$1 ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/admins
-echo -e "[boot]\nsystemd=true\n[automount]\nenabled=true\nmountFsTab=true\nroot=/mnt/\n[interop]\nenabled=true\nappendWindowsPath=true\n[user]\ndefault=$1" >> /etc/wsl.conf
+echo -e "[boot]\nsystemd=true\n\n[automount]\nenabled=true\nmountFsTab=true\nroot=/mnt/\n\n[interop]\nenabled=true\nappendWindowsPath=true\n\n[user]\ndefault=$1" >> /etc/wsl.conf
 
 # Create Bash Profile
 sudo -u $1 mkdir ~/.bash
 
 # .bash_profile
-bash_profile=$(cat<<EOF
-if [ -f ~/.profile ]; then
-	source ~/.profile
-fi
-
-if [ -f ~/.bash/bashrc ]; then
-	source ~/.bash/bashrc
-fi
-EOF
-)
-sudo -u $1 echo "$bash_profile" >> "/home/$1/.bash_profile"
+bash_profile="if [ -f ~/.profile ]; then\n\tsource ~/.profile\nfi\n\nif [ -f ~/.bash/bashrc ]; then\n\tsource ~/.bash/bashrc\nfi"
+sudo -u $1 echo -e "$bash_profile" >> "/home/$1/.bash_profile"
 
 # .bash/bashrc
 bashrc=$(cat<<EOF
@@ -31,11 +22,11 @@ source /etc/bash/git-completion.bash
 source /etc/bash/git-prompt.sh
 
 if [ -f /etc/bash_completion ]; then
-. /etc/bash_completion
+	. /etc/bash_completion
 fi
 
 export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;32m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[34m\] '
 EOF
 )
-sudo -u $1 echo "$bashrc" >> "/home/$1/.bash/bashrc"
+sudo -u $1 echo -e "$bashrc" >> "/home/$1/.bash/bashrc"
 sudo -u $1 touch "/home/$1/.profile"
