12713e419cc70faaeda151cb392da372f7f78503
Lex <58815784+avonbied@users.noreply.github.com>
2025-03-01 23:49:04 +0000

    Add GPG & GCM config

--- scripts/wsl/profileSetup.sh
+++ scripts/wsl/profileSetup.sh
@@ -2,7 +2,7 @@
 
 # Create Initial Setup
 apk update
-apk add --no-cache curl wget bash sudo gnupg bash-completion git
+apk add --no-cache curl wget bash sudo bash-completion git gpg
 curl -o /etc/bash/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o /etc/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
@@ -17,7 +17,14 @@ sed -i 's/\(^export PATH="\)/\1\$PATH:/' /etc/profile
 # GPG & GCM Config
 git config --system commit.gpgsign true
 git config --system tag.gpgSign true
+git config --system gpg.program "$(which gpg)"
+### GCM Config
 git config --system credential.helper '/mnt/c/Program\ Files\ \(x86\)/Git\ Credential\ Manager/git-credential-manager.exe'
+# curl -L https://aka.ms/gcm/linux-install-source.sh | sh && git-credential-manager configure
+# git config --system credential.helper "$(which git-credential-manager)"
+### And Uncomment these lines
+# git config --system gpg.program '/mnt/c/Program\ Files\ \(x86\)/GnuPG/bin/gpg.exe'
+# git config --global credential.credentialStore gpg
 
 # Create Bash Profile
 sudo -u $1 mkdir ~/.bash
