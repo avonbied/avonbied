82c0db7be0f1eb39791359fcecd31c840a59f904
Lex <58815784+avonbied@users.noreply.github.com>
2025-03-02 00:01:17 +0000

    Update GPG & GCM config

--- scripts/wsl/profileSetup.sh
+++ scripts/wsl/profileSetup.sh
@@ -2,29 +2,28 @@
 
 # Create Initial Setup
 apk update
-apk add --no-cache curl wget bash sudo bash-completion git gpg
+apk add --no-cache curl wget bash sudo bash-completion git
 curl -o /etc/bash/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o /etc/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
+# Add PATH for Interop
+sed -i 's/\(^export PATH="\)/\1\$PATH:/' /etc/profile
+
+# GPG Config
+git config --system commit.gpgsign true
+git config --system tag.gpgSign true
+git config --system gpg.program '/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe'
+# apk add gpg && git config --system gpg.program "$(which gpg)"
+### GCM Config
+git config --system credential.helper '/mnt/c/Program Files (x86)/Git Credential Manager/git-credential-manager.exe'
+# git config --system credential.credentialStore gpg
+# curl -L https://aka.ms/gcm/linux-install-source.sh | sh && git-credential-manager configure
+# git config --system credential.helper "$(which git-credential-manager)"
 
 # Create User
 adduser -G wheel -s /bin/bash $1
 echo -e "$1 ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/admins
 echo -e "[boot]\nsystemd=true\n\n[automount]\nenabled=true\nmountFsTab=true\nroot=/mnt/\n\n[interop]\nenabled=true\nappendWindowsPath=true\n\n[user]\ndefault=$1" >> /etc/wsl.conf
-
-# Add PATH for Interop
-sed -i 's/\(^export PATH="\)/\1\$PATH:/' /etc/profile
-
-# GPG & GCM Config
-git config --system commit.gpgsign true
-git config --system tag.gpgSign true
-git config --system gpg.program "$(which gpg)"
-### GCM Config
-git config --system credential.helper '/mnt/c/Program\ Files\ \(x86\)/Git\ Credential\ Manager/git-credential-manager.exe'
-# curl -L https://aka.ms/gcm/linux-install-source.sh | sh && git-credential-manager configure
-# git config --system credential.helper "$(which git-credential-manager)"
-### And Uncomment these lines
-# git config --system gpg.program '/mnt/c/Program\ Files\ \(x86\)/GnuPG/bin/gpg.exe'
-# git config --global credential.credentialStore gpg
 
 # Create Bash Profile
 sudo -u $1 mkdir ~/.bash
