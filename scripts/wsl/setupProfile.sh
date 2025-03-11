285a72a13fce52d2a1e3469bde4ba5369d844291
Lex <58815784+avonbied@users.noreply.github.com>
2025-03-11 18:20:17 +0000

    Fix setup script

--- scripts/wsl/setupProfile.sh
+++ scripts/wsl/setupProfile.sh
@@ -6,28 +6,20 @@ echo -e "$1 ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/admins
 echo -e "[user]\ndefault=$1" >> /etc/wsl.conf
 
 # Create Bash Profile
-sudo -u $1 mkdir ~/.bash
+mkdir "/home/$1/.bash"
 
 # .profile
-profile=$(cat<<EOF
-if [ -f ~/.bash/bashrc ]; then
+echo -e "if [ -f ~/.bash/bashrc ]; then
 	source ~/.bash/bashrc
 fi
-# export GPG_TTY=$(tty)
-EOF
-)
-sudo -u $1 echo -e "$profile" >> "/home/$1/.profile"
+# export GPG_TTY=\$(tty)" >> "/home/$1/.profile"
 
 # .bash/bashrc
-bashrc=$(cat<<EOF
-source /etc/bash/git-completion.bash
+echo -e "source /etc/bash/git-completion.bash
 source /etc/bash/git-prompt.sh
-
 if [ -f /etc/bash_completion ]; then
 	. /etc/bash_completion
 fi
+export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;32m\]\$(__git_ps1)\[\033[01;34m\] \\\$\[\033[34m\] '" >> "/home/$1/.bash/bashrc"
 
-export PS1='\[\033[01;32m\]\u@\h\[\033[01;34m\] \w\[\033[01;32m\]$(__git_ps1)\[\033[01;34m\] \$\[\033[34m\] '
-EOF
-)
-sudo -u $1 echo -e "$bashrc" >> "/home/$1/.bash/bashrc"
+chown -R $1 "/home/$1"
