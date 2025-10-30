eedaf88bd66f4132e85d27153a2c1d408e186bbf
Lex <58815784+avonbied@users.noreply.github.com>
2025-10-29 23:16:02 -0500

    Fix WSL profile setup

--- scripts/wsl/setupProfile.sh
+++ scripts/wsl/setupProfile.sh
@@ -6,7 +6,7 @@ echo -e "$1 ALL=(ALL:ALL) ALL" >> /etc/sudoers.d/admins
 echo -e "[user]\ndefault=$1" >> /etc/wsl.conf
 
 # Create Config
-curl -o- https://raw.githubusercontent.com/avonbied/avonbied/refs/heads/main/scripts/unix/profile-config.sh
+curl -o- https://raw.githubusercontent.com/avonbied/avonbied/refs/heads/main/scripts/unix/profile-config.sh | sh
 
 # .profile
 echo -e "if [ -L ~/.bashrc ]; then
