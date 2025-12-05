b762ae1e2e3de4e91cb223299de4a7fe4f51ac3e
Lex <git@avonbied.me>
2025-12-04 20:58:52 -0600

    Add pam_systemd initialization script

--- scripts/wsl/setupWsl.sh
+++ scripts/wsl/setupWsl.sh
@@ -6,10 +6,13 @@ apk add --no-cache curl wget bash sudo bash-completion git
 curl -o /etc/bash/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o /etc/bash/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
-echo -e "[boot]\nsystemd=true\n\n[automount]\nenabled=true\nmountFsTab=true\nroot=/mnt/\n\n[interop]\nenabled=true\nappendWindowsPath=true\n\n" >> /etc/wsl.conf
-
+echo -e "[boot]\nsystemd=false\n\n[automount]\nenabled=true\nmountFsTab=true\nroot=/mnt/\n\n[interop]\nenabled=true\nappendWindowsPath=true\n\n" >> /etc/wsl.conf
 # Add PATH for Interop
 sed -i 's/\(^export PATH="\)/\1\$PATH:/' /etc/profile
+
+curl -o /usr/local/bin/pam_systemd_setup https://raw.githubusercontent.com/avonbied/avonbied/refs/heads/main/scripts/wsl/utils/pam_systemd_setup
+echo -e "ALL ALL=(root) NOPASSWD: /usr/local/bin/pam_systemd_setup" >> /etc/sudoers.d/admins
+echo -e "sudo /usr/local/bin/pam_systemd_setup.sh \$USER" >> /etc/profile.d/pam_system.sh
 
 # GPG & GCM Config (Interop)
 git config --system commit.gpgsign true
@@ -17,7 +20,6 @@ git config --system tag.gpgSign true
 git config --system gpg.program '/mnt/c/Program Files (x86)/GnuPG/bin/gpg.exe'
 git config --system credential.helper '/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe'
 
-mkdir -p "/run/user/$(id -u root)"
 # GPG & GCM Config (Local)
 # apk add gpg && git config --system gpg.program "$(which gpg)"
 # git config --system credential.credentialStore gpg
