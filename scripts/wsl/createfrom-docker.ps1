2f884059bdf20861dcd6dfb41d0bb7ce0b7bd721
Lex <git@avonbied.me>
2025-12-04 20:58:08 -0600

    Fix validation for scripts

--- scripts/wsl/createfrom-docker.ps1
+++ scripts/wsl/createfrom-docker.ps1
@@ -5,23 +5,26 @@ Author: Lex von Biedenfeld <github.com/avonbied>
 param (
 	[Parameter(Mandatory=$true,
 		HelpMessage="Name of target WSL instance")]
-	[ValidateNotNullOrEmpty()]
+	[ValidatePattern("^[a-zA-Z][\w\.\-]*$")]
 	[string]$WslName,
-	[Parameter(HelpMessage="Target parent WSL directory")]
-	[string]$ParentDirectory = '.',
 	[Parameter(HelpMessage="Reference Docker image")]
-	[string]$ReferenceDockerImage = 'alpine:latest'
+	[string]$ReferenceDockerImage = 'alpine:latest',
+	[Parameter(HelpMessage="Target parent WSL directory")]
+	[string]$ParentDirectory = "$env:USERPROFILE\wsl"
 )
 
-$wslMask = $WslName.ToLower().Replace(' ','')
-$targetDataPath = "$ParentDirectory\$wslMask"
+$basePath = "$ParentDirectory\$($WslName.ToLower())"
+
+if (-not (Test-Path $basePath)) {
+	New-Item -ItemType Directory $basePath
+}
 
 # Export Docker as tar
 docker.exe pull $ReferenceDockerImage
 docker.exe run -t --name 'wsl_export' $ReferenceDockerImage ls /
-docker.exe export 'wsl_export' | Out-File 'wsl_export.tar'
+docker.exe export 'wsl_export' | Out-File 
 docker.exe rm 'wsl_export'
 
 # Import tar and cleanup
-wsl.exe --import $WslName $targetDataPath 'wsl_export.tar'
-Remove-Item 'wsl_export.tar'
+wsl.exe --import $WslName $basePath "$basePath\wsl_export.tar"
+Remove-Item "$basePath\wsl_export.tar"
