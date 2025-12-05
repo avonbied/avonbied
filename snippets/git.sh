36b65469d5c8c773de954e8398f6cdd6c25db8ec
Lex <git@avonbied.me>
2025-12-05 09:55:38 -0600

    Add alternate history change snippet

--- snippets/git.sh
+++ snippets/git.sh
@@ -15,6 +15,11 @@ git filter-branch --commit-filter 'if { [ "$GIT_AUTHOR_NAME" = "$OLD_GIT_NAME" ]
 rm -Rf .git/refs/original
 git gc --aggressive --prune=now
 
+## ALTERNATE (Modern)
+git-filter-repo --mailmap <(echo "$NEW_GIT_NAME < $NEW_GIT_EMAIL> $OLD_GIT_NAME < $OLD_GIT_EMAIL>")
+git rebase --exec 'GIT_COMMITER_DATE=$(git log -n 1 --format=%aD); git commit --amend --no-edit -n -S --date="$GIT_COMMITER_DATE"' -i --root
+
+# Merge to main/dev
 git pull; git merge --no-ff $feature -m "$commit"; git push
 
 # Get Branches with deleted remotes
